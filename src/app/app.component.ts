dbc1adafb050856903b890bd888a164d53e14181
Emircan Kirez <emircankirezz@gmail.com>
2025-02-11 19:56:48 +0300

    use subscribe method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TodoService } from '../todo-service/todo.service';
+import { Todo } from '../model/todo-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private readonly todoService: TodoService) {}
+
+  ngOnInit(): void {
+    const observable = new Observable<string>(observer => {
+      setTimeout(() => {
+        observer.next('Hello');
+        observer.next('World');
+        observer.complete();
+      }, 2000)
+    });
+    
+    console.log('just before subscribe');
+    observable.subscribe({
+      next: (x: string) => console.log('value: ' + x),
+      complete: () => console.log('completed'),
+    });
+    console.log('just after subscribe');
+  }
+
+  // after subscribing an http request, will it publish complete event?
+  getTodos() {
+    this.todoService.getTodos().subscribe({
+      next: (todoList: Todo[]) => console.log('length of the list: ' + todoList.length),
+      complete: () => console.log('completed'),
+    })
+  }
+  
 }
---
+++ src/todo-service/todo.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../model/todo-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  private readonly apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  constructor(private readonly http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]>{
+    // https://jsonplaceholder.typicode.com/todos?userId=1
+    return this.http.get<Todo[]>(this.apiUrl, {params: {userId: '1'}});
+  }
+}
